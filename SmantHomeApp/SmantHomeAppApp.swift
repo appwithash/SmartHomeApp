d9d8de75aee44996af3f321bec1ff742ba79b7fc
Ashutosh Pandey <ashutoshpandey731093@gmail.com>
2021-06-03 05:44:01 +0530

    Initial Commit

---
+++ SmantHomeApp/SmantHomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmantHomeAppApp.swift
+//  SmantHomeApp
+//
+//  Created by ashutosh on 03/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SmantHomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
